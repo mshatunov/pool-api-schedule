6c6a5a011ac44cb937d5c5ea8adab9221f7f7e05
Maxim Shatunov <mshatunov@alfabank.ru>
2018-04-25 21:21:03 +0300

    add discovery

--- src/main/java/com/mshatunov/pool/api/schedule/ScheduleApplication.java
+++ src/main/java/com/mshatunov/pool/api/schedule/ScheduleApplication.java
@@ -2,11 +2,13 @@ package com.mshatunov.pool.api.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class PoolApiScheduleApplication {
+public class ScheduleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PoolApiScheduleApplication.class, args);
+        SpringApplication.run(ScheduleApplication.class, args);
     }
 }
--- src/test/java/com/mshatunov/pool/api/schedule/ScheduleApplicationTests.java
+++ src/test/java/com/mshatunov/pool/api/schedule/ScheduleApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PoolApiScheduleApplicationTests {
+public class ScheduleApplicationTests {
 
     @Test
     public void contextLoads() {
